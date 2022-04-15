25fddde85ef17e31f44f8b7dfb34ceb86869fa54
emrealtin <emre.altin@flo.com.tr>
2022-04-15 13:22:30 +0300

    initialize

---
+++ variables.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	var a = "initial"
+	var b, c int = 1, 2
+	var d = true
+	var e int
+	f := "apple"
+
+	fmt.Println(a)
+	fmt.Println(b, c)
+	fmt.Println(d)
+	fmt.Println(e)
+	fmt.Println(f)
+
+}
